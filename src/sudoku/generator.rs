032e6012a510f76db7c884c716c0e38fde894137
MeastBlue <amalou.massinissa@gmail.com>
2023-08-09 02:36:17 +0200

    translate generator to functional

--- src/sudoku/generator.rs
+++ src/sudoku/generator.rs
@@ -6,95 +6,89 @@ pub struct Grid {
 
 impl Grid {
     pub fn new() -> Self {
-        Self {
-            grid: Self::generate_grid(),
-        }
-    }
+        let mut grid = Self { grid: [[0; 9]; 9] };
 
-    fn generate_grid() -> [[i32; 9]; 9] {
-        let mut grid = [[0; 9]; 9];
-        let mut rng = rand::thread_rng();
-
-        for row in grid.iter_mut() {
-            for col in row.iter_mut() {
-                *col = rng.gen_range(1..10);
-            }
-        }
-
-        Self::clean_grid(&mut grid);
-        grid
-    }
-
-    fn clean_grid(grid: &mut [[i32; 9]; 9]) -> &mut [[i32; 9]; 9] {
-        Self::clear_rows(grid);
-        Self::clear_columns(grid);
-        Self::clean_square(grid);
+        grid.generate_grid();
 
         grid
     }
 
-    fn clear_rows(grid: &mut [[i32; 9]; 9]) -> &mut [[i32; 9]; 9] {
-        for row in grid.iter_mut() {
+    fn generate_grid(&mut self) {
+        self.grid.iter_mut().for_each(|row| {
+            row.iter_mut().for_each(|col| {
+                *col = rand::thread_rng().gen_range(1..10);
+            })
+        });
+
+        self.clean_grid();
+    }
+
+    fn clean_grid(&mut self) {
+        self.clear_rows();
+        self.clear_columns();
+        self.clean_square();
+    }
+
+    fn clear_rows(&mut self) {
+        self.grid.iter_mut().for_each(|row| {
             let mut seen = [false; 10];
-            for col in row.iter_mut() {
-                let value = *col;
-                if value != 0 {
-                    if seen[value as usize] {
+            row.iter_mut().for_each(|col| {
+                if *col != 0 {
+                    if seen[*col as usize] {
                         *col = 0;
                     } else {
-                        seen[value as usize] = true;
+                        seen[*col as usize] = true;
                     }
                 }
-            }
-        }
-
-        grid
+            })
+        })
     }
 
-    fn clear_columns(grid: &mut [[i32; 9]; 9]) -> &mut [[i32; 9]; 9] {
-        for i_col in 0..9 {
+    fn clear_columns(&mut self) {
+        self.grid.clone().iter().enumerate().for_each(|(ic, col)| {
             let mut seen = [false; 10];
+            col.iter().enumerate().for_each(|(ir, _)| {
+                let value = self.grid[ir][ic];
 
-            for i_row in 0..9 {
-                let value = grid[i_row][i_col];
                 if value != 0 {
                     if seen[value as usize] {
-                        grid[i_row][i_col] = 0;
+                        self.grid[ir][ic] = 0;
                     } else {
                         seen[value as usize] = true;
                     }
                 }
-            }
-        }
-
-        grid
+            });
+        })
     }
 
-    fn clean_square(grid: &mut [[i32; 9]; 9]) -> &mut [[i32; 9]; 9] {
-        for row in (0..9).step_by(3) {
-            for col in (0..9).step_by(3) {
-                let mut seen = [false; 10];
+    fn clean_square(&mut self) {
+        self.grid
+            .clone()
+            .iter_mut()
+            .step_by(3)
+            .enumerate()
+            .for_each(|(ir, row)| {
+                row.iter_mut().enumerate().step_by(3).for_each(|(ic, _)| {
+                    let mut seen = [false; 10];
 
-                for i in 0..3 {
-                    for j in 0..3 {
-                        let r = row + i;
-                        let c = col + j;
-                        let value = grid[r][c];
+                    (0..3).for_each(|i| {
+                        (0..3).for_each(|j| {
+                            let r = ir + i;
+                            let c = ic + j;
+                            let value = self.grid[r][c];
 
-                        if value != 0 {
-                            let index = (value - 1) as usize;
+                            if value != 0 {
+                                let index = (value - 1) as usize;
 
-                            if seen[index] {
-                                grid[r][c] = 0;
-                            } else {
-                                seen[index] = true;
+                                if seen[index] {
+                                    self.grid[r][c] = 0;
+                                } else {
+                                    seen[index] = true;
+                                }
                             }
-                        }
-                    }
-                }
-            }
-        }
-
-        grid
+                        })
+                    })
+                })
+            })
     }
 }
